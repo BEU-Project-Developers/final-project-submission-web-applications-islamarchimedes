cdc8496bc5fd9d6a367e40d8d21bee8eb3f852a7
islamarchimedes <islesgerov001@gmail.com>
2025-05-26 01:05:03 +0400

    first commit

---
+++ JobFinderSystem/Controllers/Home2Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JobFinderSystem.Controllers
+{
+    public class Home2Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ JobFinderSystem/Models/About.cs
@@ -0,0 +1,15 @@
+﻿namespace SchoolSystem.Models
+{
+    public class About
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public string Description1 { get; set; }
+        public string Description2 { get; set; }
+        public string Description3 { get; set; }
+
+        public string ImagePath { get; set; }
+        public IFormFile Photo { get; set; }
+    }
+}
